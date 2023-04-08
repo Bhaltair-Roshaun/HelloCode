e6cd0e32994c84143652315504bb91ded37caa9c
Bhaltair <gilliarool@mail.ru>
2023-04-08 13:58:03 +0300

    07.2.Triforce

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -3,7 +3,7 @@
 //Console.WriteLine("+");
 
 
-int xa = 1, ya = 1, xb = 1, yb = 30, xc = 50, yc = 30;
+int xa = 1, ya = 1, xb = 1, yb = 15, xc = 25, yc = 15;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -14,20 +14,30 @@ Console.WriteLine("+");
 int x = xa, y = xb;
 
 int count = 0;
-int what = new Random().Next(0, 3);//0,1,2
+
 while(count < 10)
+{
+    int what = new Random().Next(0, 3);//0,1,2
+
    if (what == 0)
-{
-//это скоуп ифа
-    x = (x + xa) / 2;
-    y = (y + ya) / 2;
+    {
+        //это скоуп ифа
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+      if (what == 1)
+    {
+        //это скоуп ифа
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+    //это скоуп ифа. 
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }   
+    Console.SetCursorPosition (x,y);
+    Console.WriteLine("+");
+    count +=1;
 }
-if (what == 2)
-{
-//это скоуп ифа. 
-    x = (x+xc)/2;
-    y = (y+yc)/2;
-}
-Console.SetCursorPosition (x,y);
-Console.WriteLine("+");
-count +=1;
