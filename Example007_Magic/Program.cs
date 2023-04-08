5f3f0c56591f550d0814aa29d2e11ac1873956fd
Bhaltair <gilliarool@mail.ru>
2023-04-08 13:39:24 +0300

    07.1.Triforce

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -11,5 +11,23 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xc, yc);
+int x = xa, y = xb;
+
+int count = 0;
+int what = new Random().Next(0, 3);//0,1,2
+while(count < 10)
+   if (what == 0)
+{
+//это скоуп ифа
+    x = (x + xa) / 2;
+    y = (y + ya) / 2;
+}
+if (what == 2)
+{
+//это скоуп ифа. 
+    x = (x+xc)/2;
+    y = (y+yc)/2;
+}
+Console.SetCursorPosition (x,y);
 Console.WriteLine("+");
+count +=1;
